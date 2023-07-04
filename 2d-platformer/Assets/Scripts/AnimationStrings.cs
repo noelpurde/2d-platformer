c7eb0034dded09733056adbb9f40dde0a288a2e7
noelpurde <noelpurde@gmail.com>
2023-07-04 16:24:59 +0200

    player movement
    background and player movement to face each direction

---
+++ 2d-platformer/Assets/Scripts/AnimationStrings.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+internal class AnimationStrings
+{
+    internal static string isMoving = "isMoving";
+}
